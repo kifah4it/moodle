fbd5eb11c3ae972b02c77d9909de0893a123d9a8
kifah4it <kifah4it@outlook.com>
2024-02-23 14:25:50 +0200

    seperate registeration from enrol and reg test done

--- local/ops/classes/external/register_enroll_student.php
+++ local/ops/classes/external/register_enroll_student.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace local_ops\external;
 
 use core\session\exception;
@@ -10,96 +11,139 @@ use core_external\external_api as exterapi;
 use core_external\external_warnings;
 use stdClass;
 
-class register_enroll_student extends \core_external\external_api{
-    public static function register_enroll_student_parameters(){
+class register_enroll_student extends \core_external\external_api
+{
+    public static function enroll_student_parameters()
+    {
         return new Core_externalExternal_function_parameters(
-                array(
-                    'username' => new Core_externalExternal_value(PARAM_TEXT,'username'),
-                    'password' => new Core_externalExternal_value(PARAM_TEXT,'password'),           
-                    'firstname' => new Core_externalExternal_value(PARAM_TEXT,'first name'),       
-                    'lastname' => new Core_externalExternal_value(PARAM_TEXT,'last name'),
-                    'email' => new Core_externalExternal_value(PARAM_TEXT,'email'),
-                    'arabfullname' => new Core_externalExternal_value(PARAM_TEXT,'arabic full name'),
-                    // 'courses' => new Core_externalExternal_multiple_structure(
-                    //     new Core_externalExternal_value(PARAM_TEXT,'courseid')
-                    // )
-                    )
-                );
-        
-    }
-    public static function register_enroll_student($username, $password, $firstname, $lastname, $email, $arabfullname){
-        global $DB,$CFG;
-        // var_dump($CFG->dirroot);
-        // die();
-  //      require_once($CFG->dirroot.'/enrol/apply/lib.php');
-        // require_once("../../enrol/apply/lib.php");
-      //   $application = enrol_apply_plugin::roles_protected();
-        
-
-         
-
-        // return $result = array(
-        //     'user' => var_dump($plugin->add_instance($course,null))
-        //      );
-       $customfields = array();
-       $customfields[0] = ['type'=>'text','name'=>'profile_field_arabname','value'=>$arabfullname];
-        $result = exterapi::call_external_function('auth_email_signup_user', ['username'=> $username,'password'=>$password,
-        'firstname'=>$firstname,'lastname'=>$lastname,'email'=>$email,'customprofilefields'=>$customfields]);
-        // if user created successfully
-        if((!$result["error"] && $result["data"]["success"]) 
-        || $result["error"] && $result["exception"]->errorcode == "auth_emailnoemail"){
-            //$user = exterapi::call_external_function('core_user_get_users_by_field',['field'=>'username','values'=>[$username]]);
-            $user = $DB->get_record('user',['username'=>$username]);
-            $plugin = enrol_get_plugin('apply');
-            $course = $DB->get_record('course', array('id'=> 3), '*', MUST_EXIST);
-            $enrolmethds = $DB->get_records('enrol',array('enrol'=>'apply','courseid'=>3));
-            foreach($enrolmethds as $instance){
-            $timestart = time();
-            $timeend = $timestart + $instance->enrolperiod;
-            $plugin->enrol_user($instance,$user->id,5,$timestart,$timeend,ENROL_USER_SUSPENDED);
-            return $result = array(
-                'user' => var_dump($plugin)
-                 );
-                }
-        }
-        $result = array(
-            'user' => var_dump($result)
-             );
-        // $result = [
-        //         'fname' => $fname,
-        //         'lname' => $lname,
-        //         'arabfullname' => $arabfullname,
-        //         'username'=> $username,
-        //         'email' => $email,
-        //         'password' => $password,
-        //         'courses' => $courses,
-        //         'message' => 'success'
-        //     ];
-            return $result;
-    }
-    public static function register_enroll_student_returns(){
-       
-        return new Core_externalExternal_single_structure(
             array(
-               // 'success' => new Core_externalExternal_value(PARAM_BOOL, 'True if the user was created false otherwise'),
-                'user'  => new Core_externalExternal_value(PARAM_RAW,'user info'),
+                'username' => new Core_externalExternal_value(PARAM_TEXT, 'username'),
+                'courses' => new Core_externalExternal_multiple_structure(
+                    new Core_externalExternal_value(PARAM_TEXT, 'courseid')
+                )
             )
         );
-
-    //  return new Core_externalExternal_single_structure (   
-    //         array(
-    //         'fname' => new Core_externalExternal_value(PARAM_TEXT,'first name'),
-    //         'arabfullname' => new Core_externalExternal_value(PARAM_TEXT,'arabic full name'),
-    //         'lname' => new Core_externalExternal_value(PARAM_TEXT,'last name'),
-    //         'username' => new Core_externalExternal_value(PARAM_TEXT,'username'),
-    //         'email' => new Core_externalExternal_value(PARAM_TEXT,'email'),
-    //         'password' => new Core_externalExternal_value(PARAM_TEXT,'password'),
-    //         'message' => new Core_externalExternal_value(PARAM_TEXT,'success message'),
-    //         'courses' => new Core_externalExternal_multiple_structure(
-    //             new Core_externalExternal_value(PARAM_TEXT,'courseid')
-    //         )
-    //     ));
     }
-    
- 
+    public static function enroll_student($username, $courses = array())
+    {
+        global $DB, $CFG;
+        try {
+            $user = $DB->get_record('user', ['username' => $username]);
+            $plugin = enrol_get_plugin('apply');
+            foreach ($courses as $c) {
+                $enrolmethds = $DB->get_records('enrol', array('enrol' => 'apply', 'courseid' => $c));
+                foreach ($enrolmethds as $instance) {
+                    $timestart = time();
+                    $timeend = $timestart + $instance->enrolperiod;
+                    $plugin->enrol_user($instance, $user->id, 5, $timestart, $timeend, ENROL_USER_SUSPENDED);
+                }
+            }
+            return $result = array(
+                'status' => 'success',
+                'message' => var_dump($plugin)
+            );
+        } catch (Exception $e) {
+            return $result = array(
+                'status' => 'error',
+                'message' => $e->getMessage()
+            );
+        }
+    }
+    public static function enroll_student_returns()
+    {
+
+        return new Core_externalExternal_single_structure(
+            array(
+                // 'success' => new Core_externalExternal_value(PARAM_BOOL, 'True if the user was created false otherwise'),
+                'status'  => new Core_externalExternal_value(PARAM_RAW, 'staus'),
+                'message' => new Core_externalExternal_value(PARAM_RAW, 'error message'),
+            )
+        );
+    }
+    // Register new student
+    public static function register_student_parameters()
+    {
+        return new Core_externalExternal_function_parameters(
+            array(
+                'username' => new Core_externalExternal_value(PARAM_TEXT, 'username'),
+                'password' => new Core_externalExternal_value(PARAM_TEXT, 'password'),
+                'firstname' => new Core_externalExternal_value(PARAM_TEXT, 'first name'),
+                'lastname' => new Core_externalExternal_value(PARAM_TEXT, 'last name'),
+                'email' => new Core_externalExternal_value(PARAM_TEXT, 'email'),
+                'arabfullname' => new Core_externalExternal_value(PARAM_TEXT, 'arabic full name'),
+                'mobnum' => new Core_externalExternal_value(PARAM_TEXT, 'mobile number'),
+                'birthdate' => new Core_externalExternal_value(PARAM_TEXT, 'birthdate'),
+            )
+        );
+    }
+    public static function register_student($username, $password, $firstname, $lastname, $email, $arabfullname, $mobnum,$birthdate)
+    {
+        global $DB, $CFG;
+        $customfields = array();
+        
+        $customfields[0] = ['type' => 'text', 'name' => 'profile_field_arabname', 'value' => $arabfullname];
+        $customfields[1] = ['type' => 'text', 'name' => 'profile_field_m_num', 'value' => $mobnum];
+        $customfields[2] = ['type' => 'datetime', 'name' => 'profile_field_brthdate','value'=>strtotime($birthdate)];
+        $result = exterapi::call_external_function('auth_email_signup_user', [
+            'username' => $username, 'password' => $password,
+            'firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'customprofilefields' => $customfields
+        ]);
+
+        // if user created successfully
+        try {
+            if($result['error'] && $result["exception"]->errorcode != "auth_emailnoemail"){
+                $messages = array();
+            $messages[0] = ["field" => 'error', "message" => $result["exception"]->debuginfo];
+            $result = array(
+                'status' => 'error',
+                'messages' => $messages
+            );
+            }
+            else if ((!$result["error"] && $result["data"]["success"])
+                || $result["error"] && $result["exception"]->errorcode == "auth_emailnoemail"
+            ) {
+                $messages = array();
+                $messages[0] = ["field" => '', "message" => ''];
+                $result = array(
+                    'status' => 'success',
+                    'messages' => $messages
+                );
+            } else if (!$result["error"]["success"]) {
+                $messages = array();
+              //  $messages[0] = ["field" => 'unknown', "message" => var_dump($result)];
+                foreach ($result["data"]["warnings"] as $warn) {
+                    $messages[] = ["field" => $warn["item"], "message" => $warn["message"]];
+                }
+                $result = array(
+                    'status' => 'warnings',
+                    'messages' => $messages
+                );
+            }
+
+            return $result;
+        } catch (Exception $e) {
+            $messages = array();
+            $messages[0] = ["field" => null, "message" => $e->getMessage()];
+            $result = array(
+                'status' => 'error',
+                'messages' => $messages
+            );
+            return $result;
+        }
+    }
+    public static function register_student_returns()
+    {
+
+        return new Core_externalExternal_single_structure(
+            array(
+                'status' => new Core_externalExternal_value(PARAM_TEXT, 'success or error'),
+                'messages' => new Core_externalExternal_multiple_structure(
+                    new Core_externalExternal_multiple_structure(
+                        new Core_externalExternal_value(PARAM_TEXT, 'field'),
+                        new Core_externalExternal_value(PARAM_TEXT, 'message')
+                    )
+                ),
+            )
+        );
+    }
 }
